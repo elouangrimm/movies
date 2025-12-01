6a41bff5456e9c613e82c099476416117d190bd6
Elouan Grimm <elouan.grimm@gmail.com>
2025-12-01 20:35:17 +0100

    Change OMDb API key in config example
    Updated the example API key for OMDb in config.example.js.

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 // This is an example of how to create your own config.js file.
 // In a production environment, it is strongly recommended to use a backend proxy
 // to handle OMDb API requests and keep the API key secure.
-const OMDb_API_KEY = "YOUR_API_KEY";
+const OMDb_API_KEY = "2a319841";
