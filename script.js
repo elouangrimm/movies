3a978e38899f6ee519b66dfe437d45ea8d6d2d2f
Elouan Grimm <elouangrimm@gmail.com>
2026-02-19 11:04:00 +0100

    remove depracated video sources

--- script.js
+++
@@ -1,4 +0,0 @@
-// This file is deprecated - all functionality has been moved to modular files
-// Keeping for backward compatibility during transition
-// All search functionality is now in js/ui/search.js
-
